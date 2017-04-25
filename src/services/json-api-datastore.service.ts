0044d45d738e356e355558a002014f0099f29e51
sdzhurova <simona.dzhurova@gmail.com>
2017-04-25 12:27:30 +0300

    Update json-api-datastore.service.ts

--- src/services/json-api-datastore.service.ts
+++ src/services/json-api-datastore.service.ts
@@ -153,7 +153,7 @@ export class JsonApiDatastore {
     return model;
   }
 
-  protected handleError(error: any): ErrorObservable<T[]> {
+  protected handleError(error: any): ErrorObservable<any> {
     let errMsg: string = (error.message) ? error.message :
         error.status ? `${error.status} - ${error.statusText}` : 'Server error';
     try {
